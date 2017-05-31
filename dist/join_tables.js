6b982f1a8f4ca73d532bff9baf598311bc240476
Niko Kommenda <nkommenda@hotmail.com>
2017-05-31 09:56:43 +0100

    add binary

---
+++ dist/join_tables.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var fs = _interopDefault(require('fs'));
+var sync = _interopDefault(require('csv-parse/lib/sync'));
+var csvStringify = _interopDefault(require('csv-stringify'));
+var intoStream = _interopDefault(require('into-stream'));
+var minimist = _interopDefault(require('minimist'));
+
+const args = minimist(process.argv.slice(2));
+
+const flatten = (interm, cur) => {
+	return interm ? interm.concat(cur) : interm
+};
+
+const joinDict = (interm, cur) => {
+	return Object.assign(interm, cur)
+};
+
+
+try {
+
+	const data = 
+		args._.map(fn => {
+			return fn.endsWith('csv') ? sync(fs.readFileSync(fn), { columns : true }) : JSON.parse(fs.readFileSync(fn))
+		});
+
+	const key = args.key;
+	const keep = args.keep ? args.keep.split(',') : [];
+	const outFile = args.out;
+	const json = args.json || (outFile.endsWith('json') ? true : false);
+
+	const columns = Array.from(new Set(data.map(table => Object.keys(table[0])).reduce(flatten)));
+
+	const defaults = Object.assign(...columns.map(c => ({ [c] : '' } )));
+
+
+	const ids = Array.from(new Set(data.map(table => table.map(row => row[key])).reduce(flatten)));
+
+	const joined = ids
+		.map(id => {
+
+			return data.map(sheet => {
+				return sheet.find(row => row[key] === id)
+			}).reduce(joinDict, Object.assign({}, defaults))
+
+		})
+		.sort((a, b) => a[key] < b[key] ? -1 : 1);
+
+	if(json){
+		intoStream(JSON.stringify(joined)).pipe(args.out ? fs.createWriteStream(outFile) : process.stdout);
+	} else {
+		csvStringify(joined, { header : true }).pipe(args.out ? fs.createWriteStream(outFile) : process.stdout);
+	}
+
+} catch (err) {
+
+	console.error('Incorrect or missing arguments. Usage:');
+	console.error('join-tables [--key=KEYCOLUMN --out=OUTFILE] INFILE_1 INFILE_2 [INFILE_3 ...]');
+
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import sync from 'csv-parse/lib/sync'
 import csvStringify from 'csv-stringify'
+import intoStream from 'into-stream'
 import minimist from 'minimist'
 
 const args = minimist(process.argv.slice(2))
@@ -9,40 +10,49 @@ const flatten = (interm, cur) => {
 	return interm ? interm.concat(cur) : interm
 }
 
-const data = 
-	args._.map(fn => {
-		return fn.endsWith('csv') ? sync(fs.readFileSync(fn), { columns : true }) : JSON.parse(fs.readFileSync(fn))
-	})
-
-const key = args.key
-const keep = args.keep ? args.keep.split(',') : []
-const outFile = args.out
-const json = args.json || (outFile.endsWith('json') ? true : false)
-
 const joinDict = (interm, cur) => {
-
 	return Object.assign(interm, cur)
 }
 
-const columns = Array.from(new Set(data.map(table => Object.keys(table[0])).reduce(flatten)))
 
-const defaults = Object.assign(...columns.map(c => ({ [c] : '' } )))
+try {
+
+	const data = 
+		args._.map(fn => {
+			return fn.endsWith('csv') ? sync(fs.readFileSync(fn), { columns : true }) : JSON.parse(fs.readFileSync(fn))
+		})
+
+	const key = args.key
+	const keep = args.keep ? args.keep.split(',') : []
+	const outFile = args.out
+	const json = args.json || (outFile.endsWith('json') ? true : false)
+
+	const columns = Array.from(new Set(data.map(table => Object.keys(table[0])).reduce(flatten)))
+
+	const defaults = Object.assign(...columns.map(c => ({ [c] : '' } )))
 
 
-const ids = Array.from(new Set(data.map(table => table.map(row => row[key])).reduce(flatten)))
+	const ids = Array.from(new Set(data.map(table => table.map(row => row[key])).reduce(flatten)))
 
-const joined = ids
-	.map(id => {
+	const joined = ids
+		.map(id => {
 
-		return data.map(sheet => {
-			return sheet.find(row => row[key] === id)
-		}).reduce(joinDict, Object.assign({}, defaults))
+			return data.map(sheet => {
+				return sheet.find(row => row[key] === id)
+			}).reduce(joinDict, Object.assign({}, defaults))
 
-	})
-	.sort((a, b) => a[key] < b[key] ? -1 : 1)
+		})
+		.sort((a, b) => a[key] < b[key] ? -1 : 1)
 
-if(json){
-	fs.writeFileSync(outFile, JSON.stringify(joined))
-} else {
-	csvStringify(joined, { header : true }).pipe(fs.createWriteStream(outFile))
+	if(json){
+		intoStream(JSON.stringify(joined)).pipe(args.out ? fs.createWriteStream(outFile) : process.stdout)
+	} else {
+		csvStringify(joined, { header : true }).pipe(args.out ? fs.createWriteStream(outFile) : process.stdout)
+	}
+
+} catch (err) {
+
+	console.error('Incorrect or missing arguments. Usage:')
+	console.error('join-tables [--key=KEYCOLUMN --out=OUTFILE] INFILE_1 INFILE_2 [INFILE_3 ...]')
+
 }
